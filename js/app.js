6dec5dba437bda2479d555b04172e5460b988430
thfield <thfield@gmail.com>
2015-10-05 19:51:36 -0700

    plugins mostly there

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 
 var dataFile = dataFile || 'data/2801.json';
-var pageId = 'themap';
+var mapId = 'themap';
 var cLevels = 7;  // Number of color levels
 var colorBrew = 'YlOrRd';
 var colorPalette = colorbrewer[colorBrew][cLevels];
@@ -9,44 +9,60 @@ var valueToDraw = '';
 var quantaColors = {};
 var map;
 
-var cellWidth = 30, // Width of color legend cell
-    cbarWidth = cellWidth*cLevels;
-    cbarHeight = 10;  // Height of color legend
 
-function haitimap(dataFile){
-  d3.json(dataFile, function(error, dataset) {
-    map = new Datamap({
-      element: document.getElementById(pageId),
-      geographyConfig: {
-        dataUrl: 'data/' +  dataset.scope + '-topo05.json',
-        borderColor: '#555555',
-        popupTemplate: function(geography, data) {
-          return '<div class="hoverinfo">' + geography.properties.name +  (data ? ': ' + data[valueToDraw] : '') + '</div>';
-        }
-      },
-      scope: dataset.scope,
-      fills: {
-        defaultFill: "#fefefe"
-      },
-      data: dataset.data,
-      setProjection: function(element) {
-        var projection = d3.geo.mercator()
-          .center([-73.0513321, 19.0557096])
-          .scale(element.offsetWidth*18)
-          .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
-         var path = d3.geo.path().projection(projection);
-         return {path: path, projection: projection};
-      },
-      done: function(){
-        setColors(dataset.data);
-        colorIn(valueToDraw);
-        buildGradient(colorPalette, 'gradient');
+d3.json(dataFile, function(error, dataset) {
+  map = new Datamap({
+    element: document.getElementById(mapId),
+    geographyConfig: {
+      dataUrl: 'data/' +  dataset.scope + '-topo05.json',
+      borderColor: '#555555',
+      popupTemplate: function(geography, data) {
+        return '<div class="hoverinfo">' + geography.properties.name +  (data ? ': ' + data[valueToDraw] : '') + '</div>';
       }
-    });
-    //set title
-    d3.select('#'+pageId).append('h3').text(dataset.title);
+    },
+    scope: dataset.scope,
+    fills: {
+      defaultFill: "#fefefe"
+    },
+    data: dataset,
+    setProjection: function(element) {
+      var projection = d3.geo.mercator()
+        .center([-73.0513321, 19.0557096])
+        .scale(element.offsetWidth*18)
+        .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
+       var path = d3.geo.path().projection(projection);
+       return {path: path, projection: projection};
+    },
+    done: function(){
+      setColors(dataset.data);
+      colorIn(valueToDraw);
+      makeTitle();
+      map.choroplethKey(colorPalette,{hSize: 20, vSize: 10});
+    }
   });
-};
+
+  map.addPlugin('choroplethKey', function (layer,data,options) {
+    // hold this in a closure
+    var self = this;
+    // a class you'll add to the DOM elements
+    var className = 'choroKey';
+    // make a D3 selection.
+    var choroKey = layer
+           .selectAll(className)
+           .data( data, JSON.stringify );
+    choroKey
+      .enter()
+        .append('rect')
+        .attr('class', className) //remember to set the class name
+        .attr('width', options.hSize)
+        .attr('height', options.vSize)
+        .attr('x', function ( d, i ) { return options.hSize*i; })
+        .style('fill', function ( d ) { return d; })
+        ;
+  });
+
+});
+
 
 function setColors(dataset) {
   var vals = {};
@@ -54,11 +70,12 @@ function setColors(dataset) {
   var colorScale = {};
   var areas = d3.keys(dataset);
   var n = areas.length;
+  clearElement(mapId, 'buttons');
 
   areas.forEach(function(d){
     d3.keys(dataset[d]).forEach(function(j){
       vals[j]=vals[j] || [];
-      vals[j].push(dataset[d][j]);
+      // vals[j].push(dataset[d][j]);
       allVals.push(dataset[d][j]);
       valueToDraw = j;
     });
@@ -74,86 +91,62 @@ function setColors(dataset) {
     for (var i=0; i<n; i++) {
       quantaColors[d][areas[i]] = colorScale[d](dataset[areas[i]][d]);
     };
-    createButtons(d);
+    createButton(d);
   });
 
-  //draw key
-  visWidth = document.getElementById(pageId).offsetWidth;
-
-  cbar = d3.select('#' + pageId + ' > .datamap').append('g')
-    .attr('id', 'colorBar')
-    .attr('class', 'colorbar')
-
-  cbar.append('rect')
-      .attr('id', 'gradientRect')
-      .attr('width', cbarWidth)
-      .attr('height', cbarHeight)
-      .style('fill', 'url(#gradient)');
-
-  cbar.append('text')
-    .attr('id', 'colorBarMinText')
-    .attr('class', 'colorbar')
-    .attr('x', 0)
-    .attr('y', cbarHeight + 15)
-    .attr('dx', 0)
-    .attr('dy', 0)
-    .attr('text-anchor', 'start');
-
-  cbar.append('text')
-    .attr('id', 'colorBarMaxText')
-    .attr('class', 'colorbar')
-    .attr('x', cbarWidth)
-    .attr('y', cbarHeight + 15)
-    .attr('dx', 0)
-    .attr('dy', 0)
-    .attr('text-anchor', 'end');
-
-  cbar.attr('transform', 'translate(' + (visWidth-cbarWidth)/2.0 + ', 0)');  // Shift to center
-
-  d3.select('#gradientRect').style('fill', 'url(#gradient)');
-  d3.select('#colorBarMinText').text(d3.extent(allVals)[0]);
-  d3.select('#colorBarMaxText').text(d3.extent(allVals)[1]);
-
-};
-
-function buildGradient(palette, gradientId) {
-  d3.select('#' + pageId + ' > .datamap' )
-    .append('linearGradient')
-    .attr('id', gradientId)
-        .attr("gradientUnits", "userSpaceOnUse")
-        .attr("x1", 0)
-        .attr("y1", 0)
-        .attr("x2", cbarWidth)
-        .attr("y2", 0)
-      .selectAll('stop')
-      .data(palette)
-      .enter()
-        .append('stop')
-        .attr('offset', function(d, i) {return i/(cLevels-1)*100.0 + '%'; })
-        .attr('stop-color', function(d) {return d; });
 };
 
 function colorIn(val){
-  d3.select('#gradientRect').style('fill', 'url(#gradient)');
   map.updateChoropleth(quantaColors[val]);
   map.options.geographyConfig.popupTemplate = function(geography, data) {
       return '<div class="hoverinfo">' + geography.properties.name +  (data ? ': ' + data[val] : '') + '</div>';
   };
+
+  return false;
 };
 
-function createButtons(d){
-  var element = d3.select('#'+pageId);
+
+function loadAndRedraw(pathToFile){
+  d3.json(pathToFile, function(error,dataset){
+    map.options.data = dataset;
+    setColors(map.options.data.data);
+    colorIn(valueToDraw);
+    d3.select('.maptitle').text(dataset.title);
+  });
+};
+
+function makeTitle(){
+  d3.select('#'+mapId+' > .datamap')
+    .append('text')
+      .attr('class', 'maptitle')
+      .text(map.options.data.title)
+      .attr('dy', function(){return map.options.element.offsetHeight - 5 })
+      ;
+};
+
+function createButton(d){
+  var element = d3.select('#'+ mapId + '> .buttons');
   element.append('button')
     .attr('name', d)
     .attr('onClick', 'colorIn("' + d + '")')
     .html(d);
+  return false;
 };
 
-haitimap('data/2801.json');
-
-function clearMap(){
-  var myNode = document.getElementById("themap");
-  while (myNode.firstChild) {
-      myNode.removeChild(myNode.firstChild);
+function clearElement(elementId,className) {
+  if (className === undefined){
+    var myNode = document.getElementById(elementId);
+  } else {
+    var myNode = document.getElementById(elementId).getElementsByClassName(className)[0];
   }
+  while (myNode.firstChild) {
+    myNode.removeChild(myNode.firstChild);
+  }
+  return false;
+};
+
+function removeElement(elementId) {
+  var elem = document.getElementById(elementId);
+  elem.parentNode.removeChild(elem);
+  return false;
 }
---
+++ js/plugin.js
@@ -0,0 +1,69 @@
+var map = new Datamap({
+  scope: 'usa',
+  element: document.getElementById('container1'),
+});
+var cLevels = 7;  // Number of color levels
+var colorBrew = 'YlOrRd';
+var colorPalette = colorbrewer[colorBrew][cLevels];
+      // ["#ffffb2", "#fed976", "#feb24c", "#fd8d3c", "#fc4e2a", "#e31a1c", "#b10026"]
+
+map.addPlugin('choroplethKey', function (layer,data,options) {
+  // hold this in a closure
+  var self = this;
+
+  // a class you'll add to the DOM elements
+  var className = 'choroKey';
+
+  // make a D3 selection.
+  var choroKey = layer
+         .selectAll(className)
+         .data( data, JSON.stringify );
+
+  choroKey
+    .enter()
+      .append('rect')
+      .attr('class', className) //remember to set the class name
+      .attr('width', options.hSize)
+      .attr('height', options.vSize)
+      .attr('x', function ( d, i ) { return options.hSize*i; })
+      .style('fill', function ( d ) { return d; })
+      ;
+});
+
+
+
+map.addPlugin('bigCircle', function ( layer, data, options ) {
+  // hold this in a closure
+  var self = this;
+
+  // a class you'll add to the DOM elements
+  var className = 'bigCircles';
+
+  // make a D3 selection.
+  var bubbles = layer
+         .selectAll(className)
+         .data( data, JSON.stringify );
+
+  bubbles
+    .enter()
+      .append('circle')
+      .attr('class', className) //remember to set the class name
+      .attr('cx', function ( datum ) {
+        return self.latLngToXY(datum.lat, datum.lng)[0];
+      })
+      .attr('cy', function ( datum ) {
+        return self.latLngToXY(datum.lat, datum.lng)[1];
+      })
+      .attr('r', 10)
+      .style('fill', options.makeTheBubbleThisColor);
+ });
+
+var bubbles = [
+  {lng: -72.42, lat: 42.11},
+  {lng: -91.53, lat: 37.44},
+  {lng: -100.55, lat: 31.43}
+];
+
+// map.labels()
+map.bigCircle(bubbles, {makeTheBubbleThisColor: 'blue'}, console.log("callback"));
+map.choroplethKey(colorPalette,{hSize: 20, vSize: 10});
